f7cad2288c81b3186d5d4457621005b1466fb2db
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2022-07-11 10:33:46 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "安装Warp，请选择免费账户并调为ipv6优先级“
+wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh 6
+”调整tracker 官方论坛给出的非CFIP，绕过避免因CF导致可见真实IP"
+echo "2605:6400:20:7db:f5c7:29ce:9269:3b0a tracker.pterclub.com" > /etc/hosts
+"Finish!"
